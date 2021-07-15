a9414035b0e10850006fda6db0743f7c6c4bb9a9
mtinti <michele.tinti@gmail.com>
2021-07-15 17:46:41 +0100

    start

---
+++ web_data/E927/JS/extend_domain.js
@@ -0,0 +1,107 @@
+
+///this function try to increase the margin
+//of a plot to nicely fit all points
+function add_margin(inarray) {
+    //find the greatest absolute number
+    let max_value = Math.max.apply(null, inarray.map(Math.abs));
+    //and increase margins
+    //let margin = 0.02;
+    console.log('adjusting margins');
+    //
+    if (inarray[0] == 0 && inarray[1] > 0) {
+        inarray[0] = inarray[0] - (max_value * margin);
+        inarray[1] = inarray[1] + (max_value * margin);
+        console.log('first item 0, second item >0');
+        return inarray;
+    }
+
+    if (inarray[0] == 0 && inarray[1] < 0) {
+        inarray[0] = inarray[0] + (max_value * margin);
+        inarray[1] = inarray[1] - (max_value * margin);
+        console.log('first item 0, second item <0');
+        return inarray;
+    }
+
+    if (inarray[1] == 0 && inarray[0] > 0) {
+        inarray[1] = inarray[1] - (max_value * margin);
+        inarray[0] = inarray[0] + (max_value * margin);
+        console.log('second item 0, first item <0');
+        return inarray;
+    }
+
+    if (inarray[1] == 0 && inarray[0] < 0) {
+        inarray[1] = inarray[1] + (max_value * margin);
+        inarray[0] = inarray[0] - (max_value * margin);
+        console.log('second item 0, first item <0');
+        return inarray;
+    }
+
+    if (inarray[0] > 0 && inarray[1] > 0) {
+        if (inarray[0] < inarray[1]) {
+            inarray[0] = inarray[0] - (max_value * margin)
+            inarray[1] = inarray[1] + (max_value * margin)
+            console.log('both > 0');
+            return inarray;
+        }
+    }
+
+    if (inarray[0] < 0 && inarray[1] < 0) {
+        if (inarray[0] > inarray[1]) {
+            inarray[0] = inarray[0] + (max_value * margin)
+            inarray[1] = inarray[1] - (max_value * margin)
+            console.log('both < 0');
+            return inarray;
+        }
+
+    }
+
+    if (inarray[0] < 0 && inarray[1] > 0) {
+
+        inarray[0] = inarray[0] - (max_value * margin)
+        inarray[1] = inarray[1] + (max_value * margin)
+        console.log('second item >0, first item <0');
+        return inarray;
+
+
+    }
+
+    if (inarray[0] > 0 && inarray[1] < 0) {
+
+        inarray[0] = inarray[0] + (max_value * margin)
+        inarray[1] = inarray[1] - (max_value * margin)
+        console.log('second item <0, first item >0');
+        return inarray;
+
+
+    }
+
+}
+
+function test_extend(){
+var domain = [0, 100];
+console.log(add_margin(domain));
+var domain = [0, -100];
+console.log(add_margin(domain));
+var domain = [100, 0];
+console.log(add_margin(domain));
+var domain = [-100, -0];
+console.log(add_margin(domain));
+var domain = [0.1, 10];
+console.log(add_margin(domain));
+var domain = [10, 0.1];
+console.log(add_margin(domain));
+var domain = [-0.1, -10];
+console.log(add_margin(domain));
+var domain = [-10, -0.1];
+console.log(add_margin(domain));
+var domain = [-10, 10];
+console.log(add_margin(domain));
+var domain = [10, -10];
+console.log(add_margin(domain));
+}
+
+
+
+
+
+//test_extend()
---
+++ web_data/E927/config.js
@@ -0,0 +1,29 @@
+let X1='bsf_median';
+let Y1='pcf_median';
+let X2='rank_bsf_median';
+let Y2='rank_pcf_median';
+
+let FLIP_Y1=false;
+let FLIP_Y2=false;
+
+let PLOT_WIDTH=550;
+let PLOT_HEIGHT=400;
+
+let PROJECT_TITLE='iBAQ tBrucei';
+
+let margin = 0.05;
+
+let BOX_PLOT_COLS_1 = ['BSF_1', 'BSF_10', 'BSF_11', 'BSF_12', 'BSF_13', 'BSF_14', 'BSF_15', 'BSF_16', 'BSF_17', 'BSF_18', 'BSF_19', 'BSF_2', 'BSF_20', 'BSF_21', 'BSF_3', 'BSF_4', 'BSF_5', 'BSF_6', 'BSF_7', 'BSF_8', 'BSF_9']
+let BOX_PLOT_COLS_2 = ['PCF_1', 'PCF_10', 'PCF_11', 'PCF_12', 'PCF_13', 'PCF_14', 'PCF_15', 'PCF_16', 'PCF_17', 'PCF_18', 'PCF_19', 'PCF_2', 'PCF_20', 'PCF_21', 'PCF_22','PCF_23', 'PCF_24', 'PCF_25', 'PCF_26', 'PCF_27', 'PCF_3', 'PCF_4', 'PCF_5', 'PCF_6', 'PCF_7', 'PCF_8', 'PCF_9']
+
+let CONDITION_1 = 'BSF'
+let CONDITION_2 = 'PCF'
+
+
+let COLOR_SELECTION_TABLE = 'orange';
+let COLOR_SELECTION_CIRCLE = 'orange';
+
+let Y_LABEL_BARPLOT = 'Intensity';
+
+let File_1 = 'indata.csv';
+let File_2 = 'indata2.csv';
